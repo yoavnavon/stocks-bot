cd1460b1225059221109975d84e45e3d23dc0f97
Yoav Navon <yanavon@uc.cl>
2021-04-28 23:24:30 -0400

    Add charts

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 from dotenv import load_dotenv
+import yfinance as yf
+import matplotlib
+matplotlib.use('AGG') 
+import matplotlib.pyplot as plt
+import boto3
+from botocore.exceptions import NoCredentialsError
+import uuid
+
 load_dotenv()
 PORT = int(os.environ.get('PORT', '8443'))
 
@@ -17,11 +25,47 @@ TOKEN = os.environ['TOKEN']
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text('Hi there!')
 
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
+
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=os.environ['ACCESS_KEY'],
+                      aws_secret_access_key=os.environ['SECRET_KEY'])
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+
+
+
+def data(update, context):
+  
+    msft = yf.Ticker("MSFT")
+
+    # get stock info
+    print(msft.info)
+
+    # get historical market data
+    hist = msft.history(period="5d")
+
+    hist['Close'].plot(figsize=(16, 9))
+    plt.savefig('image.png')
+    aws_file = f'{uuid.uuid4()}.png'
+    aws_url = f'https://stocks-bot.s3-sa-east-1.amazonaws.com/{aws_file}'
+    
+    upload_to_aws('image.png', 'stocks-bot', aws_file)
+    update.message.reply_photo(aws_url)
 
 def echo(update, context):
     """Echo the user message."""
@@ -44,6 +88,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("chart", data))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
@@ -55,7 +100,8 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN,
-                          webhook_url="https://fathomless-springs-83910.herokuapp.com/" + TOKEN
+                          # webhook_url="https://fathomless-springs-83910.herokuapp.com/" + TOKEN
+                          webhook_url="https://c5d9c4eef9d3.ngrok.io/" + TOKEN
                           )
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
