c4eaa6dc99f77274591413fc7054abe38534a775
Yoav Navon <yanavon@uc.cl>
2021-05-01 11:37:50 -0400

    Add volume plots

--- app.py
+++ app.py
@@ -84,12 +84,25 @@ def plot(update, context):
     hist['index_str'] = hist.index_str.apply(lambda label: label[5:-9])
 
     # Plot
-    _, ax = plt.subplots(figsize=(12, 8))
-    sns.lineplot(x='index_str', y='Close', data=hist, ax=ax)
-    ax.set_xticks(np.linspace(0, len(hist)-1, num=5, dtype=int))
-    ax.set_xlabel(ticker)
-    ax.set_ylabel('value')
-    ax.tick_params(axis='x', rotation=30)
+    _, valueax = plt.subplots(figsize=(12, 8))
+    sns.lineplot(x='index_str', y='Close', data=hist, ax=valueax)
+    valueax.set_xticks(np.linspace(0, len(hist)-1, num=5, dtype=int))
+    valueax.set_xlabel(ticker)
+    valueax.set_ylabel('value')
+    valueax.tick_params(valueaxis='x', rotation=30)
+    valueax.set_ylim([hist['Close'].min() * 0.98, hist['Close'].max()])
+
+    volumeax = valueax.twinx()
+    sns.barplot(x='index_str', y='Volume', data=hist,
+                ax=volumeax, color="salmon")
+    volumeax.tick_params(
+        axis='y',
+        right=False,
+        labelright=False)
+    volumeax.set_xticks(np.linspace(0, len(hist)-1, num=5, dtype=int))
+    volumeax.set_ylim([0, 5 * hist['Volume'].max()])
+    volumeax.set_ylabel('')
+
     plt.savefig('image.png')
     plt.clf()
 
